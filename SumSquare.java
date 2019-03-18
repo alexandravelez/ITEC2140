f7dd02ef4f96ce561223be296587be878abb5696
Paulo9ja <48529572+Paulo9ja@users.noreply.github.com>
2019-03-18 14:04:50 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,15 +1,13 @@
 
-public class SumSquare 
-{
-	public static void main (String [] args)
-	{
+public class SumSquare {
+	public static void main (String [] args){
+		
 		int sum = 0; 
 		int multiple = 3;
 		int i = 2;
 		int result = 0;
 		
-		while (i <= 12)
-		{
+		while (i <= 12) {
 			 sum = (int) Math.pow(multiple, 2) + result; 
 			
 			//sum = multiple * i;
