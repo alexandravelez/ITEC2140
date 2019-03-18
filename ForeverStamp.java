70f139411c328f65e57078491054d7399566a68e
alexandravelez <48529618+alexandravelez@users.noreply.github.com>
2019-03-18 14:45:54 -0400

    Update ForeverStamp.java

--- ForeverStamp.java
+++ ForeverStamp.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-public class ForeverStamp { 
-	public static void main (String[] args) { 
-		
+public class ForeverStamp 
+{ 
+	public static void main (String[] args) 
+	{ 
 		//Prompts the user to enter the weight in ounces
 		Scanner input = new Scanner (System.in);
 		System.out.print("Enter weights in ounce for letters to mail out.\n When done entering, please enter -1 to finish: ");
@@ -17,6 +18,7 @@ public class ForeverStamp {
 				System.out.print("Enter weights in ounce for letters to mail out. \nWhen done entering, please enter -1 to finish: ");
 				ounce = input.nextDouble();
 			}
+			
 			//Tells the user that they need an additional postage since their weight is over 1
 			else 
 			{
